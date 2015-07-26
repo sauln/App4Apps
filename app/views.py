ff27aa2bd9e4222f49a763de7e617260a446583d
Miles Saul <mileshsaul@yahoo.com>
2015-07-25 20:57:59 -0700

    Removed extra features not related to app4apps and added script to get corpus from google play store

--- app/views.py
+++ app/views.py
@@ -2,22 +2,10 @@ from app import app, render_template
 import markov.transition_builder as tran
 import markov.chain_builder as chain
 
-
-
-
 @app.route('/index')
 @app.route('/')	
-def home():
-	return render_template("home_page.html")
-
-
-
-
 @app.route('/gen')
 def generator():
-	
-	generated_text = chain.buildChain(
-		tran.firstOrder, tran.secondOrder, tran.first)
-
-	return render_template("generator.html", generated_text=generated_text)
+    generated_text = chain.buildChain(tran.firstOrder, tran.secondOrder, tran.first)
+    return render_template("generator.html", generated_text=generated_text)
 
--- scraper/scraper/spiders/app_spider.py
+++ scraper/scraper/spiders/app_spider.py
@@ -1,13 +1,22 @@
 import scrapy
 from scraper.items import AppItem
 
+
+
 class AppSpider(scrapy.Spider):
     name = "apper"
     allowed_domains = ["play.google.com"]
     start_urls = ["https://play.google.com/store/apps/category/FINANCE"]
 	
     def parse(self, response):
+        f = open('../corpus/corpus.txt', 'w')
         for sel in response.xpath('//*[contains(@class, "description")]'):
             item = AppItem()
             item['desc'] = sel.xpath('text()').extract()[0]
             print item['desc']
+            try: 
+                f.write(item['desc'] + '\n')
+            except:
+                continue
+        
+        f.close()
