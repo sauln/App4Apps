2c820c4bd3ee4221ff796549b0d049be74968837
Miles Saul <mileshsaul@yahoo.com>
2015-07-26 12:36:46 -0700

    Updated corpus to get information from a different page

--- scraper/scraper/spiders/app_spider.py
+++ scraper/scraper/spiders/app_spider.py
@@ -6,7 +6,7 @@ from scraper.items import AppItem
 class AppSpider(scrapy.Spider):
     name = "apper"
     allowed_domains = ["play.google.com"]
-    start_urls = ["https://play.google.com/store/apps/category/FINANCE"]
+    start_urls = ["https://play.google.com/store/apps/category/FINANCE/collection/topselling_paid"]
 	
     def parse(self, response):
         f = open('../corpus/corpus.txt', 'w')
