7af39de9d78f5ce920ec54d665356add9ea338a2
Miles Saul <mileshsaul@yahoo.com>
2015-07-26 18:58:16 -0700

    Added script that collects all descriptions for all categories of app descriptions

---
+++ scraper/collect_data.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+./run_spider.sh business
+./run_spider.sh communication
+./run_spider.sh education 
+./run_spider.sh entertainment 
+./run_spider.sh finance 
+./run_spider.sh medical 
+./run_spider.sh photography 
+./run_spider.sh social 
+./run_spider.sh sports 
--- scraper/scraper/spiders/app_spider.py
+++ scraper/scraper/spiders/app_spider.py
@@ -9,16 +9,22 @@ class AppSpider(scrapy.Spider):
 
     def __init__(self, category=""):
         self.category=category
-        self.start_urls = ["https://play.google.com/store/apps/category/" + category.upper() + "/collection/topselling_paid"]
+        self.start_urls = [
+            "https://play.google.com/store/apps/category/" + category.upper() + "/collection/topselling_paid", 
+            "https://play.google.com/store/apps/category/" + category.upper() + "/collection/topselling_free"
+        ]
+        self.f = open('../corpus/corpus_' + self.category.lower() + '.txt', 'w')
+        self.urls_parsed = 0
+
     def parse(self, response):
-        f = open('../corpus/corpus_' + self.category.lower() + '.txt', 'w')
         for sel in response.xpath('//*[contains(@class, "description")]'):
             item = AppItem()
             item['desc'] = sel.xpath('text()').extract()[0]
             print item['desc']
             try: 
-                f.write(item['desc'] + '\n')
+                self.f.write(item['desc'] + '\n')
             except:
                 continue
-        
-        f.close()
+        self.urls_parsed += 1
+        if self.urls_parsed > 1:
+            self.f.close()
