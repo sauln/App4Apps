c7032358355d30bae9a8f52bf491a66b1f367919
Nathaniel Saul <nat@saulgill.com>
2016-01-17 13:40:03 -0800

    Radio buttons render with Bootstrap CSS, have default, and remember previous settings

--- tests.py
+++ tests.py
@@ -1,7 +1,11 @@
 from django.test import TestCase
 from django.core.urlresolvers import resolve
+from unittest import skip
+
 
 from app4apps.views import app_page
+from app4apps.services import text_gen
+
 
 class App4AppsTest(TestCase):
 	def test_app4app_url_resolves_to_app4app_view(self):
@@ -9,4 +13,18 @@ class App4AppsTest(TestCase):
 		self.assertEqual(found.func, app_page)
 
 
+class TextGenServiceTest(TestCase):
+
+
+	def test_returns_text(self):
+		text = text_gen.new()
+		assertEqual(text, "Random Text")
+	
+	@skip
+	def test_returns_different_text(self):
+		pass
+	
+	@skip
+	def test_returns_text_associated_to_category(self):
+		pass
 
--- views.py
+++ views.py
@@ -6,10 +6,12 @@ def app_page(request):
 	if request.method == 'POST':
 		form = OptionsRadio(request.POST)
 		if form.is_valid():
-			print(form) 
+			print(form)
 			value = form.cleaned_data['option']
-		return render(request, 'app4app.html', {'fauxText': "you selected %s"%value, 'form':form,})
+		return render(request, 'app4app.html', {'fauxText': "you selected %s"%value, 'form':form, 'category':value})
 	else:
 		form = OptionsRadio()
-	return render(request, 'app4app.html', {'fauxText': "Hi How's it going", 'form':form,})
+
+	print(form)
+	return render(request, 'app4app.html', {'fauxText': "Hi How's it going", 'form':form, 'category':'business'})
 
