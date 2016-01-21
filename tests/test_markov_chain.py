f4aef326949fbebc16179e24c85f69921d6636dc
Nathaniel Saul <nat@saulgill.com>
2016-01-20 23:46:22 -0800

    Tests for each chain_maker function.  Complete refactor of chain_maker implementation.  Much better, less naive

--- tests/test_markov_chain.py
+++ tests/test_markov_chain.py
@@ -36,7 +36,7 @@ class MarkovChainTest(TestCase):
 	def test_chain_returns_text(self):
 		text = chain.text_from_chain(self.mc)		
 		self.assertIsInstance(text, str)
-
+	
 	def test_chain_is_length_50_words(self):
 		desired_length = 50
 		text = chain.text_from_chain(self.mc, desired_length)
@@ -44,11 +44,41 @@ class MarkovChainTest(TestCase):
 			"Length of text is not 50, but instead %s"%len(text.split()))
 
 
-class ChainMakerTest(TestCase):
 
+class ChainMakerTest(TestCase):
 	def setUp(self):
 		self.mc = chain.load_markov_chain("business")
+		self.d1 = {'a':1, 'b':2, 'c':3}
+		self.d2 = {'b':2, 'c':1, 'd':4}
+		self.d3 = {'a':1, 'b':4, 'c':3, 'd':4}
 
-	@skip
+	def tearDown(self):
+		self.d1 = None
+		self.d2 = None
+		self.d3 = None
+		self.mc = None
+
 	def test_merge(self):
-		pass
+		res = self.mc.merge(self.d1,self.d2)
+		self.assertEqual(self.d3, res)	
+
+	def test_getNextWord(self):
+		word = self.mc.getNextWord(self.d3)
+		self.assertIn(word, self.d3.keys())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- training/markov/chain_maker.py
+++ training/markov/chain_maker.py
@@ -4,15 +4,12 @@ Created on Sun Jul 26 20:17:54 2015
 
 @author: nathaniel
 
-
-
-
 This class is responsible for generating the markov chain.
-
 It contains the necessary frequency dictionaries and functions, and that's it.
+It is to be trimmed down version of the markov chain that
+has only the essential components for generating the chain.
 
-Meant to be trimmed down version essential for the site.
-
+To train (create the dictionaries) look at chain_trainer.py.
 
 """
 
@@ -37,44 +34,65 @@ class markov_state():
 
 		newText = first_word + " " + second_word
 
-		for _ in range(length):
-			newD = self.merge(first_word, second_word)
+		for _ in range(length-2):
+			newD = self.merge(self.first_order[first_word], self.second_order[second_word])
 	  
 			first_word = second_word	
 			second_word = self.getNextWord(newD)
 		  
-		newText += " " + second_word
+			newText += " " + second_word
 		return newText
 
-	def merge(self, word2, word1):
-		''' This will merge the first and second order dictionaries of the specific words'''
-		newD = self.mergeLists(self.first_order[word1], self.second_order[word2])
-		return newD
-	def mergeLists(self,list1, list2):
+
+	def merge(self,list1, list2):
+		''' merges two dictionaries into 3rd ''' 
+		''' why multiplies counts instead of adds? '''
+		
 		newDict = dict()
-		''' create a new dictionary of frequencies based of two inputs ''' 
-	   
-		for each in list1:
-			if each in list2:
-				newDict[each] = list1[each]*list2[each]
+		union_keys = set(list1.keys()).union(set(list2.keys()))
+		for each in union_keys:
+			newDict[each] = (list1[each] if each in list1.keys() else 1) * \
+				(list2[each] if each in list2.keys() else 1)
+
+		#print("List1:%s\nList2:%s\nNewList:%s"%(list1, list2, newDict))
 		return newDict
 
 	def getNextWord(self, newD):
 		''' Given a dictionary of words and frequencies, 
 		this will generate a new word by random number '''
-	    
 		
+		possibles = []
 		
-		total = 0
-		newTotal = 0
-		for each in newD:
-			total += newD[each]
-            
-		index = random.randint(1, total)
-    	
-		for each in newD:
-			newTotal += newD[each]
-			if newTotal >= index:
-				return each
-			
+		#flatmap the dictionary out
+		for (a,b) in zip(newD.keys(), newD.values()):
+			possibles += [a]*b 
+	
+		success = 0
+
+		#this type of checking is not my favorite style
+		#when we know why owa (maybe others) are causing problems,
+		#we can get rid of this block.
+		while(success == 0):
+
+			#choose a random element in the list
+			index = random.randint(0, len(possibles)-1)
+			word = possibles[index]	
+
+			#make sure this word is legit
+			try: 
+				self.first_order[word]
+				self.second_order[word]
+				success = 1
+			except:
+				success = 0
+
+		return word
+
+
+
+
+
+
+
+
 		
